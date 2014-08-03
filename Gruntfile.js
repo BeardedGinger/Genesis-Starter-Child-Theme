4445563bc5185d1b76d6c52fe627a25b44be7f58
BeardedGinger <josh@limecuda.com>
2014-08-03 15:05:59 -0400

    Updating starter theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
             },
             livereload: {
                 options: { livereload: true },
-                files: ['scss/**/*.{scss,sass}','style.css', '**/*.html', '**/*.php', 'images/**/*.{png,jpg,jpeg,gif,webp,svg}']
+                files: ['style.css', '**/*.html', '**/*.php', 'images/**/*.{png,jpg,jpeg,gif,webp,svg}']
             }
         },
 
--- functions.php
+++ functions.php
@@ -11,13 +11,27 @@ add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_scripts' );
 /**
  * Enqueue scripts and fonts
  *
+ * Loads full scripts in local development when WP_DEBUG is turned on.
+ *
+ * Run 'grunt uglify' to update minified scripts as well as create conditional page scripts.
+ * Before pushing to server, run 'grunt uglify' to update uglified version
+ *
  * @since 1.0.0
  */
 function enqueue_child_theme_scripts() {
-	
+
 	wp_enqueue_style( 'google-font-lato', '//fonts.googleapis.com/css?family=Lato:300,700', array(), CHILD_THEME_VERSION );
-	
-	wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/js/plugins.min.js', array( 'jquery' ), CHILD_THEME_VERSION );
+
+	if( WP_DEBUG ) {
+		wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ) );
+		wp_enqueue_script( 'mobile-nav', get_stylesheet_directory_uri() . '/assets/js/mobile-nav.js', array( 'jquery' ) );
+	} else {
+		if( is_home() ) {
+			wp_enqueue_script( 'home-scripts', get_stylesheet_directory_uri() . '/js/plugins.home.min.js', array( 'jquery' ), CHILD_THEME_VERSION );
+		} else {
+			wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/js/plugins.min.js', array( 'jquery' ), CHILD_THEME_VERSION );
+		}
+	}
 }
 
 //* Add HTML5 markup structure
