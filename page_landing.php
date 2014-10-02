8c4781fbfdb3e3acbbc50a160bbc37c5c5a769de
Josh Mallard <josh@limecuda.com>
2014-10-02 09:21:12 -0400

    Update page_landing.php

--- page_landing.php
+++ page_landing.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  *
@@ -47,53 +46,4 @@ remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
 
 //* Run the Genesis loop
 genesis();
-=======
-<?php
-/**
- *
- * Template Name: Landing Page
- *
- * Default landing page for starter theme
- *
- * @since 1.0.0
- */
-
-add_filter( 'body_class', 'limecuda_landing_body_class' );
-/**
- * Add "landing-page" body class to the body for this template
- *
- * @since 1.0.0
- */
-function limecuda_landing_body_class( $classes ) {
-
-	$classes[] = 'landing-page';
-	return $classes;
-
-}
-
-//* Force full width content layout
-add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
-
-//* Remove site header elements
-remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
-remove_action( 'genesis_header', 'genesis_do_header' );
-remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
-
-//* Remove navigation
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-
-//* Remove breadcrumbs
-remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
-
-//* Remove site footer widgets
-remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
-
-//* Remove site footer elements
-remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
-remove_action( 'genesis_footer', 'genesis_do_footer' );
-remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
-
-//* Run the Genesis loop
-genesis();
->>>>>>> 12034551094d432295bcfa489487d2e850bb9393
+?>
